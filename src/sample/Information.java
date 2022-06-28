5429fcd79555c048afa0ae54340dc630b6a400eb
MashaGarkaeva <77572567+MashaGarkaeva@users.noreply.github.com>
2022-06-28 16:47:58 +0300

    Update Information.java

--- src/sample/Information.java
+++ src/sample/Information.java
@@ -1,10 +1,8 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 
 import java.io.*;
 
